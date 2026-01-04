63705355e9f7c1ee83d3614645ea54dd7261db3f
David Johnson <davejohnson3000@gmail.com>
2026-01-04 00:29:35 -0800

    initial release of ml-lxc

---
+++ ct/ml.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.plex.tv/
+
+APP="Plex"
+var_tags="${var_tags:-media}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-ubuntu}"
+var_version="${var_version:-24.04}"
+var_unprivileged="${var_unprivileged:-1}"
+var_gpu="${var_gpu:-yes}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/apt/sources.list.d/plexmediaserver.list ]] &&
+    [[ ! -f /etc/apt/sources.list.d/plexmediaserver.sources ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if [ "$UPD" == "1" ]; then
+    msg_info "Updating ${APP} LXC"
+    $STD apt update
+    $STD apt -y upgrade
+    msg_ok "Updated ${APP} LXC"
+    msg_ok "Updated successfully!"
+    exit
+  fi
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:32400/web${CL}"
